373c82d5f574c49efd8e2165267d9f70edebc70f
Tanuj612 <tanujmittal2018@gmail.com>
2025-06-11 12:08:54 +0530

    second

---
+++ sum.java
@@ -0,0 +1,13 @@
+// package week01-Core-Programming;
+import java.util.Scanner;
+
+public class sum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b =scanner.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+    
+}
