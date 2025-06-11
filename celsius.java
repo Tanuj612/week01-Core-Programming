99e9b76f703955474da671d1e38941d020985b5a
Tanuj612 <tanujmittal2018@gmail.com>
2025-06-11 12:12:13 +0530

    third

---
+++ celsius.java
@@ -0,0 +1,10 @@
+// package week01-Core-Programming;
+
+public class celsius {
+    public static void main(String[] args) {
+        double fahrenheit = 100.0;
+        double celsius = (fahrenheit - 32) * 5 / 9;
+        System.out.println("The temperature in Celsius is: " + celsius);
+    }
+    
+}
