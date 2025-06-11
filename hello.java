937c14048b571a813bbc7e50182104e65bef77ab
Tanuj612 <tanujmittal2018@gmail.com>
2025-06-11 12:29:25 +0530

    Update variable initialization and calculation in main method

--- hello.java
+++ hello.java
@@ -1,6 +1,11 @@
 public class hello {
     public static void main(String[] args) {
-        int a=1;
+        int a=10;
+        int b=20;
+        int c = a + b;
         System.out.println("Hello, World");
+
+
+        // dfrurufug
     }
 }
